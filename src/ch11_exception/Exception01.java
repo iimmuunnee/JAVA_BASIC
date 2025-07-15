33998ca10fb1aa9c744786571bbf2591728e460c
Keep going <51280476+iimmuunnee@users.noreply.github.com>
2025-07-15 12:07:31 +0900

    feat: Update Exception Code

--- src/ch11_exception/Exception01.java
+++ src/ch11_exception/Exception01.java
@@ -15,10 +15,43 @@
  *  - 예외는 2가지 종류
  *    1) 일반 예외: 컴파일러 체크 → 컴파일러 오류 발생
  *    2) 실행 예외: 개발자 경험에 의해서 예외 처리 코드 삽입
+ *  - DB, I/O 반드시 예외처리를 꼭 해줘야 함
+ *  
+ * ■ 예외 종류
+ *  1. NullPointerException
+ *  2. ArrayIndexOutBoundsException
+ *  3. NumberFormatException
+ *  4. ClassCastException
+ *  5. ...
+ *  
+ * ■ 예외처리 종류
+ * 1. try ~ catch ~ finally (예외 처리)
+ * 2. throw (예외 강제발생: 의도적으로 예외를 강제로 발생)
+ * 3. throws (예외 전가: 예외를 처리하지 않고 다음으로 넘김)
+ * 4. 사용자 정의 예외 (직접 예외를 만들어서 사용)
+ * 
+ * ■ 예외처리 문법
+ *  1. try: 예외가 발생 할 수도 있는 부분
+ *  2. catch: 예외가 발생했을 때 처리하는 부분
+ *  3. finally: 예외가 발생하든 안 하든 실행되는 부분, 생략가능
+ *   - 자원 반납 (해제)
  */
 
 package ch11_exception;
 
 public class Exception01 {
-
+	public void test1() {
+		try {
+			// 예외가 발생할 수도 있는 부분
+		} catch(NullPointerException e) {
+			
+		} catch(ClassNotFoundException e) {
+			
+		} catch(Exception e) {
+			// 예외처리 하는 부분
+		} finally {
+			// 예외가 발생하든 안 하든 실행하는 부분
+			// ex) 자원 해제
+		}
+	}
 }
